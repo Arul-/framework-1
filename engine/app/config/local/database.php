0a3dfc537f8cf7bfda83598e60edda6993f0fda8
Anas Anjaria <anasanjaria@gmail.com>
2015-03-25 21:40:35 +0500

    Building framework

---
+++ engine/app/config/local/database.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sadia
+ * Date: 4/11/14
+ * Time: 5:29 PM
+ */
+
+return array(
+'connections' => array(
+    'mysql' => array(
+//        'driver'    => 'mysql',
+        'host'      => $_ENV['DB.HOST'],
+//        'database'  => 'test',
+        'username'  => $_ENV['DB.USERNAME'],
+        'password'  => $_ENV['DB.PASSWORD'],
+//        'charset'   => 'utf8',
+//        'collation' => 'utf8_unicode_ci',
+//        'prefix'    => '',
+    ),
+
+));
---
+++ engine/app/controllers/Users.php
@@ -0,0 +1,54 @@
+<?php
+
+class Users {
+
+    function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return array
+     */
+    function get($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * @status 201
+     *
+     * @param string $name  {@from body}
+     * @param string $email {@type email} {@from body}
+     *
+     * @return mixed
+     */
+    function post($name, $email)
+    {
+        return 'adding new record . ' . $name . ' - ' . $email ;
+    }
+
+    /**
+     * @param int    $id
+     * @param string $name  {@from body}
+     * @param string $email {@type email} {@from body}
+     *
+     * @return mixed
+     */
+    function put($id, $name, $email)
+    {
+        return 'update record . ' . $id . $name . ' - ' . $email ;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return array
+     */
+    function delete($id)
+    {
+        return 'deleting record . ' . $id ;
+    }
+}
